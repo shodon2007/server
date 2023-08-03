51c3602e043e374304cf0c26f0029f76fbeb5b3c
shodon <about@shodon.ru>
2023-08-03 21:03:23 +0700

    ts

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors');
-const path = require('path');
+// const cors = require('cors');
+// const path = require('path');
 
-const logger = require('./middlewares.js');
-const routes = require('./routes/routes.js');
+// const logger = require('./middlewares.js');
+// const routes = require('./routes/routes.js');
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.static(path.resolve(__dirname, 'static')));
-app.use(express.json());
-app.use(cors());
-app.use(logger);
-app.use('/apps', routes);
+// app.use(express.static(path.resolve(__dirname, 'static')));
+// app.use(express.json());
+// app.use(cors());
+// app.use(logger);
+// app.use('/apps', routes);
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
 
 function start() {
     try {
